abe521c0f0c2a4a40733f0ec05d5021c08fa6235
SorcoDispatch <sorcodispatch@gmail.com>
2025-05-22 14:46:30 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
 
-self.addEventListener('install', function(event) {
+const CACHE_NAME = "tank-calc-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./company_logo.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('tank-calc-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/company_logo.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
